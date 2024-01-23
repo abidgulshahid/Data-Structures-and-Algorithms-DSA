18c5ba7501c427bd8aeaa73c7fdb041dfa1d10c6
Abid Gul Shahid <abidgulshahid@gmail.com>
2024-01-24 03:20:26 +0500

    Update queue.py

--- Python/queue.py
+++ Python/queue.py
@@ -9,6 +9,10 @@ enter q to stop the program
 while True:
     take_input = input("Enter Value (q to stop): ") 
     if take_input == 'q': break
-    array.append(take_input)
+    elif take_input == 'd': 
+        print('Current Array: ',array)
+        array.pop(0)
+    else:array.append(take_input)
+    
 
 print(array)
