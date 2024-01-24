ebf525b32ba951c4cdc6553a05165630b99983f4
Abid Gul Shahid <abidgulshahid@gmail.com>
2024-01-25 03:27:52 +0500

    Update binary_search.py

--- Python/binary_search.py
+++ Python/binary_search.py
@@ -1,28 +1,12 @@
 
-def binary_search(array, value):
+def binary_search(array, value): 
+    l = 0
+    h = len(array)
+    while l <= h:
+        mid = (l + h) / 2 
+        midpoint = array[int(mid)]
+        print(mid, midpoint)
+        break;
 
-    # first to know the length of a array
-    lower_bound = 0
-    high_bound = len(array) - 1 
-    print(high_bound)
-    while high_bound <= lower_bound: 
-        print(high_bound, lower_bound)
-        midpoint = (lower_bound + high_bound) / 2
-        midpoint = int(midpoint)
-        print(midpoint, 'midpount')
-        value_at_midpoint = array[midpoint]
-        print(value_at_midpoint, 'value at midpoint')
-        
-        if value < value_at_midpoint: 
-            print(high_bound, midpoint , 'if')
-            high_bound = midpoint + 1
-        elif value > value_at_midpoint: 
-
-            lower_bound = midpoint - 1
-        
-        elif value == value_at_midpoint:
-            return value
-        break
-        
-array = [2,4,5,7,8,9,66]
-print(binary_search(array=array, value=9))
+array = [5,7, 9,13,32,33 ,42, 54, 56, 88]
+print(binary_search(array=array, value= 42))
