daef23bd05645cb87bb1f26dc368fc0eeb3f71db
Abid Gul Shahid <abidgulshahid@gmail.com>
2024-01-27 22:58:25 +0500

    stack go

--- Go/stack.go
+++ Go/stack.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func stack(array [5]int) {
+	fmt.Println("Stack here")
+	for i := 0; i <= len(array); i++ {
+		fmt.Print("Remove Value? (y/n):")
+		var input string
+		fmt.Scan(&input)
+		fmt.Print(input)
+
+		fmt.Scan(input)
+	}
+}
 
 func main() {
-	fmt.Println("1+2 =")
+	stack_array := [5]int{1, 2, 3, 4, 5}
+	stack(stack_array)
 
 }
