c36ca361db7a4c35f2e922fb6eaea1b95ba0ba7b
Abid Gul Shahid <abidgulshahid@gmail.com>
2024-02-21 22:13:40 +0500

    Update bubble_sort.py

--- Python/bubble_sort.py
+++ Python/bubble_sort.py
@@ -1,17 +1,12 @@
 def bubble_sort(list): 
     unsorted_until_index = len(list) - 1
-    print(unsorted_until_index, 'unsorted_until_index')
     sorted = False
     while not sorted:
         sorted = True
-        print(sorted, 'sorted')
         for i in range(unsorted_until_index):
-            print(list[i],  list[i+1], 'list[i] > list[i+1]')
             if list[i] > list[i+1]:
                 sorted = False
-                print(list, 'before')
                 list[i], list[i+1] = list[i+1], list[i]
-                print("AFTER ", list)
         unsorted_until_index = unsorted_until_index - 1
 
 
