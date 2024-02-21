57967af1a78e2675046f25ffcb9f5db8d260e08b
Abid Gul Shahid <abidgulshahid@gmail.com>
2024-02-21 18:12:14 +0500

    Create o_of_n.py

---
+++ Python/o_of_n.py
@@ -0,0 +1,9 @@
+
+array = ['Abid', 'Gul', "Shahid", 'Marwat']
+for i in array:
+    print("Array Items: ", i)
+
+
+"""
+The time complexitiy of this algorithm is O(N) because as we increase data the steps of the alogiritm also increases
+"""
