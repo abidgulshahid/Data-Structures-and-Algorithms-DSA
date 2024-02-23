f1a57dc9a512c67fd6662b3c370660b79cb5b6b5
Abid Gul Shahid <abidgulshahid@gmail.com>
2024-02-24 02:10:04 +0500

    Update quadratic_problem.py

--- Python/quadratic_problem.py
+++ Python/quadratic_problem.py
@@ -1,11 +1,15 @@
 
 def hasDuplicateValue(array):
+    steps = 0
     for i in range(len(array)): 
         for j in range( len(array)):
+            steps = steps + 1
             print(i, j, array[i] , array[j])
             if i != j and array[i] == array[j]:
+                print(steps)
                 return True
+    print(steps)
     return False
 
-arr = [1,2,3,4,5,6,6 ]
+arr = [1,2,3,4,5,6 ]
 print(hasDuplicateValue(array=arr))
